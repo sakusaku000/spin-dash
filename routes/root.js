d55e492df2810dbda0db76982ecf27b74cde0acf
sakusaku000 <harry.spncr@gmail.com>
2023-05-15 09:16:01 +0100

    changed root page response

--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-    return res.status(200).send()
+    return res.status(200).send("spin-dash")
 })
 
 module.exports = router;
